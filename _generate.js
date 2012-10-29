cd64cbf3474a7e9969513a3bd1cac62587512094
Tobias Koppers <tobias.koppers@googlemail.com>
2012-10-29 22:26:41 +0100

    generate local, better styling

--- _generate.js
+++ _generate.js
@@ -20,40 +20,49 @@ var count = files.length;
 files.forEach(function(file) {
 	fs.readFile(file.mdFile, "utf-8", function(err, mdContent) {
 		if(err) throw err;
-		
-		var reqContent = JSON.stringify({
-			text: mdContent,
-			mode: "gfm",
-			context: "jmpressjs/docs"
-		});
-		
-		var opt = require("url").parse("https://api.github.com/markdown");
-		opt.headers = {
-			accept: "application/vnd.github.beta+json",
-			host: "api.github.com",
-			"user-agent": "NodeJS HTTP Client",
-			"content-length": reqContent.length,
-			"content-type": "application/json"
-		};
-		opt.method = "POST";
-		var req = https.request(opt, function(res) {
-			res.setEncoding("utf-8");
-			if(res.statusCode != 200) {
-				console.error("statusCode should be 200, but is " + res.statusCode);
-				console.dir(res.headers);
-				return;
-			}
-			var arr = [];
-			res.on("data", arr.push.bind(arr));
-			res.on("end", function() {
-				onResponse(arr.join(""));
+
+		try {
+			var marked = require("marked");
+			marked.setOptions({
+				gfm: true,
+				pedantic: true
 			});
-		}).on("error", function(err) {
-			console.error(err);
-		});
-	
-		req.write(reqContent, "utf-8");
-		req.end();
+			onResponse(marked(mdContent));
+		} catch(e) {
+			var reqContent = JSON.stringify({
+				text: mdContent,
+				mode: "gfm",
+				context: "jmpressjs/docs"
+			});
+
+			var opt = require("url").parse("https://api.github.com/markdown");
+			opt.headers = {
+				accept: "application/vnd.github.beta+json",
+				host: "api.github.com",
+				"user-agent": "NodeJS HTTP Client",
+				"content-length": reqContent.length,
+				"content-type": "application/json"
+			};
+			opt.method = "POST";
+			var req = https.request(opt, function(res) {
+				res.setEncoding("utf-8");
+				if(res.statusCode != 200) {
+					console.error("statusCode should be 200, but is " + res.statusCode);
+					console.dir(res.headers);
+					return;
+				}
+				var arr = [];
+				res.on("data", arr.push.bind(arr));
+				res.on("end", function() {
+					onResponse(arr.join(""));
+				});
+			}).on("error", function(err) {
+				console.error(err);
+			});
+
+			req.write(reqContent, "utf-8");
+			req.end();
+		}
 	});
 
 	function onResponse(html) {
